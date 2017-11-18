243a81b012b81947e704806363e872c6e9d0d992
zhengxiang zhong <31775919+zzxiang8752@users.noreply.github.com>
2017-11-18 14:06:31 -0500

    Updating profile page

---
+++ profile_page/profile.js
@@ -0,0 +1,11 @@
+(function(){
+    var config = {
+        apiKey: "AIzaSyC3FpISPgUilSgSmpm-pjOUPlRp8nYwm5Q",
+        authDomain: "pause-fb19e.firebaseapp.com",
+        databaseURL: "https://pause-fb19e.firebaseio.com",
+        projectId: "pause-fb19e",
+        storageBucket: "pause-fb19e.appspot.com",
+        messagingSenderId: "714359295912"
+      };
+    firebase.initializeApp(config);
+});
