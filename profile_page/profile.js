ab0c66fc978422c93af730c63d7c8b75db4e1101
Anish <nkanish2002@gmail.com>
2017-11-18 16:33:32 -0500

    Adding username and profile pic

--- profile_page/profile.js
+++ profile_page/profile.js
@@ -1,28 +1,34 @@
-var config = {
-    apiKey: "AIzaSyC3FpISPgUilSgSmpm-pjOUPlRp8nYwm5Q",
-    authDomain: "pause-fb19e.firebaseapp.com",
-    databaseURL: "https://pause-fb19e.firebaseio.com",
-    projectId: "pause-fb19e",
-    storageBucket: "pause-fb19e.appspot.com",
-    messagingSenderId: "714359295912"
-};
-firebase.initializeApp(config);
-check_if_logged_in();
+$(function(){
+    var config = {
+        apiKey: "AIzaSyC3FpISPgUilSgSmpm-pjOUPlRp8nYwm5Q",
+        authDomain: "pause-fb19e.firebaseapp.com",
+        databaseURL: "https://pause-fb19e.firebaseio.com",
+        projectId: "pause-fb19e",
+        storageBucket: "pause-fb19e.appspot.com",
+        messagingSenderId: "714359295912"
+    };
+    firebase.initializeApp(config);
+    check_if_logged_in();
+    window.user = null;
 
-function goto_main_page()   {
-    window.location.href = "/index.html";
-}
+    function goto_main_page()   {
+        window.location.href = "/index.html";
+    }
 
-function check_if_logged_in()   {
-    debugger;
-    setTimeout(function(){
-        if(firebase.auth().currentUser == null)    {
-            goto_main_page();
-        }
-    }, 300);
-}
+    function check_if_logged_in()   {
+        setTimeout(function(){
+            if(firebase.auth().currentUser == null)    {
+                goto_main_page();
+            } else {
+                window.user = firebase.auth().currentUser;
+                $("#username").html(window.user.displayName);
+                $("#user-img").attr('src', window.user.photoURL);
+            }
+        }, 300);
+    }
 
-$("#sign-out").click(function(){
-    firebase.auth().signOut();
-    goto_main_page();
+    $("#sign-out").click(function(){
+        firebase.auth().signOut();
+        goto_main_page();
+    });
 });
