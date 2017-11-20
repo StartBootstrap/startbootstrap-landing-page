98951e86afb42e7bf748d73c6d8b7fb9caec3cc6
swarnimsinha <31718689+swarnimsinha@users.noreply.github.com>
2017-11-19 22:18:29 -0500

    Update if exists
    Using the inbuilt update if exists rather than checking the number of rows

--- database/insertData.php
+++ database/insertData.php
@@ -10,20 +10,18 @@ $total_duration = $_POST['total_duration'];
 $last_viewed = $date = date('Y-m-d H:i:s');
 
 // make the entry for the user if it doesn't exist
-$sql1 = "SELECT * FROM `user` WHERE `user_id` = '$user_id'";
-$result1 = $conn->query($sql1);
-echo $sql1;
-if ($result1->num_rows == 0) {
-    $sql = "INSERT INTO `user`(`user_id`, `email`, `name`, `last_login`) VALUES ('$user_id', 'swarnims@bu.edu', 'Swarnim', '$last_viewed')";
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        echo $sql;
-    }
+
+$sql = "INSERT INTO `user`(`user_id`, `email`, `name`, `last_login`) VALUES ('$user_id', 'swarnims@bu.edu', 'Swarnim', '$last_viewed')
+ON DUPLICATE KEY UPDATE `last_login` = '$last_viewed'";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo $sql;
 }
 
-$sql = "INSERT INTO `video`(`video_id`, `user_id`, `url`, `video_progress`, `total_duration`, `last_viewed`) VALUES ('0', '$user_id', '$url', '$video_progress', '$total_duration', '$last_viewed')";
+$sql = "INSERT INTO `video`(`video_id`, `user_id`, `url`, `video_progress`, `total_duration`, `last_viewed`) VALUES ('0', '$user_id', '$url', '$video_progress', '$total_duration', '$last_viewed')
+ON DUPLICATE KEY UPDATE `video_progress` = '$video_progress', `last_viewed` = '$last_viewed'";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
