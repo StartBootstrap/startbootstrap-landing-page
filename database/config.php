f4bf8982ad546bd48d5d0eb97d8b3bc55d6bdc7a
swarnimsinha <31718689+swarnimsinha@users.noreply.github.com>
2017-11-19 22:53:35 -0500

    Remove echo
    Remove the code used for debugging

--- database/config.php
+++ database/config.php
@@ -10,5 +10,4 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 ?>
