7e19a95b44991f61b11a6e0667bff6fab48494d2
David Miller <hello@davidmiller.io>
2021-04-02 14:13:07 -0400

    project restructure removing gulp
    update to bootstrap 4.6, remove gulp, restructure project, update dependencies

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Landing Page v5.1.0 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
