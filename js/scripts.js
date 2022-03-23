01a07ca97422408e10e9ee830649bdcc31e35fd7
David Miller <hello@davidmiller.io>
2022-03-23 12:46:08 -0400

    bump version

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Landing Page v6.0.4 (https://startbootstrap.com/theme/landing-page)
+* Start Bootstrap - Landing Page v6.0.5 (https://startbootstrap.com/theme/landing-page)
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
 */
