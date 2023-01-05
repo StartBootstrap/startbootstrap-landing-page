d10feed1c8b788a98b81229a2e0a9438cc6bc1c9
notsag-dev <a.gaston.gimenez@gmail.com>
2023-01-05 13:48:03 -0300

    Update bootstrap icons to latest version

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Landing Page v6.0.5 (https://startbootstrap.com/theme/landing-page)
-* Copyright 2013-2022 Start Bootstrap
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
 */
 // This file is intentionally blank
